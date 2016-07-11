7efc348794efff4e28cca133fe751f5963923e3a
buyongfeng521 <849001270@qq.com>
2016-07-11 22:21:49 +0800

    fix:jpg image process

--- JanDanGirl.py
+++ JanDanGirl.py
@@ -5,12 +5,12 @@ import json
 import re
 import string
 import HTMLParser
-import xlwt
 import uuid
 from bs4 import BeautifulSoup
 
 jandan_url = "http://jandan.net/ooxx/page-"
-img_basic_path = r'G:/Ex/PythonEx/PyImage/JanDanImages/'
+#img_basic_path = r'G:/Ex/PythonEx/PyImage/JanDanImages/'
+img_basic_path = r'G:/Images/JanDan/'
 
 
 def processData(beginPage,endPage):
@@ -24,16 +24,19 @@ def processData(beginPage,endPage):
 		listContent = soup("a",class_="view_img_link")
 		for item in listContent:
 			imgHref = str(item['href'])
-			strImg = str(uuid.uuid1()) + ".jpg"
-			if ".gif" in imgHref:
+			if ".jpg" in imgHref:
 				strImg = str(uuid.uuid1()) + ".jpg"
+				down_load_img(item['href'],strImg)
+			# if ".gif" in imgHref:
+			# 	strImg = str(uuid.uuid1()) + ".gif"
+			# 	down_load_img(item['href'],strImg)
 
-			down_load_img(item['href'],strImg)
+			
 
 
 def load_data(url):
-	user_agent="Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"
-	# user_agent="Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10"
+	#user_agent="Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"
+	user_agent="Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10"
 	headers = {"User-Agent": user_agent}
 
 	req = urllib2.Request(url, headers = headers)
@@ -47,8 +50,9 @@ def down_load_img(url,imgName):
 	data = urllib.urlretrieve(url,path)
 
 
+
 def main():
-	processData(2040,2048)
+	processData(200,210)
 
 if __name__ == "__main__":
 	main()
